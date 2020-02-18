809d57efa23f1481ce5714c9a21d28b31d3df1ca
LucaGuettinger <luca.guettinger@cmiag.ch>
2020-02-18 16:34:26 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,6 @@
+function getSize(fieldId) {
+    return document.getElementById(fieldId).nodeValue.length;
+}
+function setText(element, content) {
+    document.getElementById(element).innerText = content;
+}
---
+++ test.ts
@@ -0,0 +1,12 @@
+function getSize(fieldId: string): number {
+	return document.getElementById(fieldId).nodeValue.length;
+}
+function setText(element: string, content: string): void {
+	document.getElementById(element).innerText = content;
+}
+
+
+class Abc {
+
+
+}
